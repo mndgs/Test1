ae52dc66327b37f160a09a8a1e1280aab055fe2c
Mindaugas Paukste <mindaugaspaukste@macbook.local>
2019-03-18 21:44:02 +0200

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Mindaugas Paukste on 18/03/2019.
+//  Copyright © 2019 Mindaugas Paukste. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
