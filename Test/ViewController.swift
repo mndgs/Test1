5de1a3d46b245c9fe284f2735725539020f9fd12
Mindaugas Paukste <mindaugaspaukste@MacBook.local>
2019-03-21 23:08:48 +0200

    1

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -2,19 +2,64 @@
 //  ViewController.swift
 //  Test
 //
-//  Created by Mindaugas Paukste on 18/03/2019.
-//  Copyright © 2019 Mindaugas Paukste. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class StackViewController: UIViewController {
+    
+    let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        return stackView
+    }()
+    
+    lazy var navBar: UINavigationBar = {
+        let navBar = UINavigationBar()
+        navBar.translatesAutoresizingMaskIntoConstraints = false
+        navBar.delegate = self
+        navBar.pushItem(UINavigationItem(title: "Title"), animated: false)
+        return navBar
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .blue
+        
+        viewDidLoad1()
+//      viewDidLoad2()
     }
-
-
+    
+    func viewDidLoad1() {
+        view.addSubview(stackView)
+        
+        stackView.addArrangedSubview(navBar)
+        // jei true - top marginas  👍
+        stackView.isLayoutMarginsRelativeArrangement = true
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            stackView.topAnchor.constraint(equalTo: view.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            stackView.widthAnchor.constraint(equalTo: view.widthAnchor)
+            ])
+    }
+    
+    func viewDidLoad2() {
+        view.addSubview(navBar)
+        
+        NSLayoutConstraint.activate([
+            navBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            navBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            navBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            ])
+    }
+    
 }
 
+extension StackViewController: UINavigationBarDelegate {
+    func position(for bar: UIBarPositioning) -> UIBarPosition {
+        return .topAttached
+    }
+}
